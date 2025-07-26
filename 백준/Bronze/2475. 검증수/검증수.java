8c753032d6f06435a5b0f1da9bd59662c68b7b60
hyukkimm <163751877+hyukkimm@users.noreply.github.com>
2025-07-26 15:16:54 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum=0;
+        int a =  Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        sum = a*a+b*b+c*c+d*d+e*e;
+        System.out.println(sum%10);
+    }
+}
